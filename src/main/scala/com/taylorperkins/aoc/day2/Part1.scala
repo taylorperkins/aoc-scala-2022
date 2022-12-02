bb2643120990be4982543ceaffdb10ad106c5a0e
Taylor Perkins <taylorperkins@iheartmedia.com>
2022-12-02 08:26:18 -0600

    Part 1

--- src/main/scala/com/taylorperkins/aoc/day2/Part1.scala
+++ src/main/scala/com/taylorperkins/aoc/day2/Part1.scala
@@ -1,10 +1,48 @@
 package com.taylorperkins.aoc.day2
 import com.taylorperkins.aoc.com.taylorperkins.aoc.core.Utils
 
+
+
 object Part1 extends App
   with Utils
 {
-  using(resource("src/main/resources/day1.txt")) { input => {
-    println(input.getLines().toList)
+  object RPSEnum extends Enumeration {
+    type Object = Value
+    val Rock, Paper, Scissors = Value
+  }
+
+  case class RPSValue(label: RPSEnum.Object, value: Int, win: RPSEnum.Object, loss: RPSEnum.Object) {
+    def play(other: RPSValue): Int = other.label match
+      case `label`   => 3 + value
+      case `win`     => 6 + value
+      case `loss`    => 0 + value
+  }
+
+  val rock = RPSValue(RPSEnum.Rock, 1, RPSEnum.Scissors, RPSEnum.Paper)
+  val paper = RPSValue(RPSEnum.Paper, 2, RPSEnum.Rock, RPSEnum.Scissors)
+  val scissors = RPSValue(RPSEnum.Scissors, 3, RPSEnum.Paper, RPSEnum.Rock)
+
+  implicit def toRPS(s: String): RPSValue = s match {
+    case "A" | "X" => rock
+    case "B" | "Y" => paper
+    case "C" | "Z" => scissors
+  }
+
+  // Some sanity checks
+  //  for {
+  //    left <- Array("A", "B", "C")
+  //    right <- Array("X", "Y", "Z")
+  //  } println(left.play(right))
+
+  using(resource("src/main/resources/day2.txt")) { input => {
+
+    val result = input.getLines
+      .map(line => {
+        val Array(a, b) = line.split(" ")
+        b.play(a)
+      })
+      .sum
+
+    println(result)
   }}
 }
