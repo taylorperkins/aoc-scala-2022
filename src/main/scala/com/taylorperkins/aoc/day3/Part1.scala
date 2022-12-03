bb2c6836528446138a2d6d2f054b239b7d29e590
Taylor Perkins <taylorperkins@iheartmedia.com>
2022-12-03 11:44:00 -0600

    day3 part 1

--- src/main/scala/com/taylorperkins/aoc/day3/Part1.scala
+++ src/main/scala/com/taylorperkins/aoc/day3/Part1.scala
@@ -1,10 +1,21 @@
 package com.taylorperkins.aoc.day3
 import com.taylorperkins.aoc.com.taylorperkins.aoc.core.Utils
 
+import scala.collection.immutable.HashSet
+
 object Part1 extends App
   with Utils
 {
-  using(resource("src/main/resources/day1.txt")) { input => {
-    println(input.getLines().toList)
+  val ids = ('a' to 'z') ++ ('A' to 'Z')
+
+  using(resource("src/main/resources/day3.txt")) { input => {
+    val result = input.getLines
+      .map(str => str.grouped(str.length / 2).toList match {
+        case List(a, b) => a.toSet.intersect(b.toSet).head
+      })
+      .map(ids.indexOf(_) + 1)
+      .sum
+
+    println(result)
   }}
 }
