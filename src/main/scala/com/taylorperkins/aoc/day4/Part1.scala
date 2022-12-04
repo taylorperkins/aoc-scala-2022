b493af009571569e54fcff2e95b57ade3b8418a6
Taylor Perkins <taylorperkins@iheartmedia.com>
2022-12-04 08:13:34 -0600

    Pt 1

--- src/main/scala/com/taylorperkins/aoc/day4/Part1.scala
+++ src/main/scala/com/taylorperkins/aoc/day4/Part1.scala
@@ -4,7 +4,20 @@ import com.taylorperkins.aoc.com.taylorperkins.aoc.core.Utils
 object Part1 extends App
   with Utils
 {
-  using(resource("src/main/resources/day1.txt")) { input => {
-    println(input.getLines().toList)
+
+  using(resource("src/main/resources/day4.txt")) { input => {
+    val result = input.getLines
+      .map(line => {
+        val Array(left, right) = line.split(",")
+        val Array(a, b) = left.split("-").map(_.toInt)
+        val Array(y, z) = right.split("-").map(_.toInt)
+
+        if (a <= y && b >= z) 1
+        else if (y <= a && z >= b) 1
+        else 0
+      })
+      .sum
+
+    println(result)
   }}
 }
