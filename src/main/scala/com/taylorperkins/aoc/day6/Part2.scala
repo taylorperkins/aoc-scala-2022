c443807c535371c7f736b42bd42ba26c4fa02816
Taylor Perkins <taylorperkins@iheartmedia.com>
2022-12-06 07:47:20 -0600

    day 2

--- src/main/scala/com/taylorperkins/aoc/day6/Part2.scala
+++ src/main/scala/com/taylorperkins/aoc/day6/Part2.scala
@@ -1,10 +1,24 @@
 package com.taylorperkins.aoc.day6
 import com.taylorperkins.aoc.com.taylorperkins.aoc.core.Utils
 
+import scala.annotation.tailrec
+
 object Part2 extends App
   with Utils
 {
-  using(resource("src/main/resources/day1.txt")) { input => {
-    println(input.getLines().toList)
+
+  @tailrec
+  def findPacketMarker(line: String, acc: Int = 0): Int = {
+    if (line.take(14).toSet.size == 14) acc+14
+    else findPacketMarker(line.drop(1), acc+1)
+  }
+
+
+  using(resource("src/main/resources/day6.txt")) { input => {
+
+    input.getLines
+      .map(line => findPacketMarker(line, 0))
+      .foreach(println)
+
   }}
 }
